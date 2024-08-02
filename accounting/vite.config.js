b555a51c29b0f187d6da662a9c25870bc48f95a6
i <fdgh@fgh.yyy>
2024-08-02 15:43:25 +0300

    fix config

--- accounting/vite.config.js
+++ accounting/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/api',
+        target: 'https://accounting-tir9.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
