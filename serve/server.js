4cd172ba9152b692a2e3ea349277d2b7f092be18
Q <yarik.kabashov@yandex.ru>
2024-08-04 21:33:00 +0300

    final project

--- serve/server.js
+++ serve/server.js
@@ -4,16 +4,18 @@ const dataRoutes = require('./routes/dataRoutes');
 const cors = require('cors');
 
 app.use(express.json());
-app.use('/api', dataRoutes);
 
-
-// Используйте CORS middleware
 app.use(cors({
     origin: 'https://accounting-smoky.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+
+app.use('/api', dataRoutes);
+
+
+// Используйте CORS middleware
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
