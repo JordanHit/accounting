ca2bf309942e518cbce7a34af151aa5b27bfda3f
Q <yarik.kabashov@yandex.ru>
2024-08-04 20:56:32 +0300

    final project

--- serve/server.js
+++ serve/server.js
@@ -8,9 +8,11 @@ app.use('/api', dataRoutes);
 
 
 // Используйте CORS middleware
-app.use(cors({
-    origin: 'https://accounting-smoky.vercel.app' // Укажите ваш клиентский адрес
-}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://accounting-smoky.vercel.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
