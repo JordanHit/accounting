8940ac91e21ce06b465aa38d3578c87f9e309403
Q <yarik.kabashov@yandex.ru>
2024-08-04 19:52:59 +0300

    final project

--- serve/db/db.js
+++ serve/db/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'postgresql://q1010001q:tdEofwb2PxpK4T9LRhSDqfq62Z9UYXaL@dpg-cqnqo6ggph6c73b0m820-a/accounting_fwsr',
+    host: 'dpg-cqnqo6ggph6c73b0m820-a',
     user: 'q1010001q',
     password: 'tdEofwb2PxpK4T9LRhSDqfq62Z9UYXaL',
     database: 'accounting_fwsr',
