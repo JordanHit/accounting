0142bf26aafcd40e16bbe2173c5a8121fe7d6619
i <fdgh@fgh.yyy>
2024-08-06 11:10:49 +0300

    fix config

--- serve/server.js
+++ serve/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: 'https://accounting-smoky.vercel.app',
+        origin: 'https://centrgorki.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization']
     },
